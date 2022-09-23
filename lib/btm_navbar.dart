05b47e3fc85598cdc631330d4eaac1f5ffffc3c7
Vinayak Ranjane <vinayakranjane2501@gmail.com>
2022-09-23 12:19:40 +0530

    Bottom Nav Bar Added

---
+++ lib/btm_navbar.dart
@@ -0,0 +1,80 @@
+import 'package:commect/screens/ChatScreen/chat_screen.dart';
+import 'package:flutter/material.dart';
+
+class BottomNavBar extends StatefulWidget {
+  const BottomNavBar({super.key});
+
+  @override
+  State<BottomNavBar> createState() => _BottomNavBarState();
+}
+
+class _BottomNavBarState extends State<BottomNavBar> {
+  int _selectedIndex = 0;
+  static List<Widget> _widgetOptions = <Widget>[
+    Container(
+      child: Center(
+        child: Text("Home Page"),
+      ),
+    ),
+    Container(
+      child: Center(
+        child: Text("Events Page"),
+      ),
+    ),
+    ChatScreen(),
+    Container(
+      child: Center(
+        child: Text("Profile Page"),
+      ),
+    ),
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        // backgroundColor: Color(0xff252525),
+        bottomNavigationBar: BottomNavigationBar(
+          showSelectedLabels: false,
+          type: BottomNavigationBarType.fixed,
+          showUnselectedLabels: false,
+          selectedItemColor: const Color(0xff6200ee),
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.home,
+              ),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.play_arrow_rounded,
+              ),
+              label: 'Events',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.message_rounded,
+              ),
+              label: 'Chats',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.person,
+              ),
+              label: 'Profile',
+            ),
+          ],
+          currentIndex: _selectedIndex,
+          onTap: (index) {
+            setState(() {
+              _selectedIndex = index;
+            });
+          },
+        ),
+        body: Center(
+          child: _widgetOptions.elementAt(_selectedIndex),
+        ),
+      ),
+    );
+  }
+}
